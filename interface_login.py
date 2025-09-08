cfa20d95e9f08733e51b26b02bffb9318fc6c86e
Gamerk <caduka678@gmail.com>
2025-09-08 20:36:33 -0300

    -Transformar o código inteiro em uma função

--- interface_login.py
+++ interface_login.py
@@ -1,44 +1,45 @@
-import customtkinter as ctk
+def interface_login():
+    import customtkinter as ctk
 
-# configuração da aparência
-ctk.set_appearance_mode('dark')
-# criação da janela prncipal
-app = ctk.CTk()
-app.title('Login Sala do Futuro')
-app.geometry('300x300')
+    # configuração da aparência
+    ctk.set_appearance_mode('dark')
+    # criação da janela prncipal
+    app = ctk.CTk()
+    app.title('Login Sala do Futuro')
+    app.geometry('300x300')
 
-# criaçã das funções de funcionalidades
-def validar_login():
-    usuario =  entrada_usuario.get()
-    senha = entrada_senha.get()
+    # criaçã das funções de funcionalidades
+    def validar_login():
+        usuario =  entrada_usuario.get()
+        senha = entrada_senha.get()
 
-    # verificar usuário e senha
-    if usuario == 'Carlos' and senha == '1234':
-        resultado_login.configure(text='Seja bem vindo Carlos bonitão😉',text_color='green')
+        # verificar usuário e senha
+        if usuario == 'Carlos' and senha == '1234':
+            resultado_login.configure(text='Seja bem vindo Carlos bonitão😉',text_color='green')
 
-    else:
-        resultado_login.configure(text='Login ou senha inválidos!',text_color='red')
+        else:
+            resultado_login.configure(text='Login ou senha inválidos!',text_color='red')
 
-# criação dos campos
-# label
-label_usuario = ctk.CTkLabel(app,text='Usuário(RA):')
-label_usuario.pack(pady = 10)
-# entry
-entrada_usuario = ctk.CTkEntry(app,placeholder_text='Digite seu RA')
-entrada_usuario.pack(pady=10)
-# label
-label_senha = ctk.CTkLabel(app,text='Senha:')
-label_senha.pack(pady = 10)
-# entry
-entrada_senha = ctk.CTkEntry(app,placeholder_text='Digite sua senha')
-entrada_senha.pack(pady=10)
-#button
-botao = ctk.CTkButton(app,text='Logar',command=validar_login)
-botao.pack(pady=10)
+    # criação dos campos
+    # label
+    label_usuario = ctk.CTkLabel(app,text='Usuário(RA):')
+    label_usuario.pack(pady = 10)
+    # entry
+    entrada_usuario = ctk.CTkEntry(app,placeholder_text='Digite seu RA')
+    entrada_usuario.pack(pady=10)
+    # label
+    label_senha = ctk.CTkLabel(app,text='Senha:')
+    label_senha.pack(pady = 10)
+    # entry
+    entrada_senha = ctk.CTkEntry(app,placeholder_text='Digite sua senha')
+    entrada_senha.pack(pady=10)
+    #button
+    botao = ctk.CTkButton(app,text='Logar',command=validar_login)
+    botao.pack(pady=10)
 
-# campo do feedback do login
-resultado_login = ctk.CTkLabel(app,text='')
-resultado_login.pack(pady=10)
+    # campo do feedback do login
+    resultado_login = ctk.CTkLabel(app,text='')
+    resultado_login.pack(pady=10)
 
-# inicia o loop da aplicação
-app.mainloop()
+    # inicia o loop da aplicação
+    app.mainloop()
