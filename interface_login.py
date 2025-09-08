33b20b8e2ee1ffd46d61bd4cb4a819913792a5c1
Gamerk <caduka678@gmail.com>
2025-09-08 20:58:15 -0300

    -Transformar o código inteiro em uma função
    -Refatorei o código, mudando algumas variáveis e deixando mais legível

--- interface_login.py
+++ interface_login.py
@@ -1,6 +1,7 @@
 def interface_login():
     import customtkinter as ctk
 
+
     # configuração da aparência
     ctk.set_appearance_mode('dark')
     # criação da janela prncipal
@@ -8,9 +9,11 @@ def interface_login():
     app.title('Login Sala do Futuro')
     app.geometry('300x300')
 
-    # criaçã das funções de funcionalidades
+
+    # ----- criação das funções de funcionalidades -----
     def validar_login():
-        usuario =  entrada_usuario.get()
+        # validar login
+        usuario =  entrada_ra.get()
         senha = entrada_senha.get()
 
         # verificar usuário e senha
@@ -20,26 +23,28 @@ def interface_login():
         else:
             resultado_login.configure(text='Login ou senha inválidos!',text_color='red')
 
-    # criação dos campos
-    # label
-    label_usuario = ctk.CTkLabel(app,text='Usuário(RA):')
-    label_usuario.pack(pady = 10)
-    # entry
-    entrada_usuario = ctk.CTkEntry(app,placeholder_text='Digite seu RA')
-    entrada_usuario.pack(pady=10)
-    # label
-    label_senha = ctk.CTkLabel(app,text='Senha:')
-    label_senha.pack(pady = 10)
-    # entry
+    #----- criação dos campos -----
+    #campo usuário
+    ctk.CTkLabel(app,text='Usuário(RA):').pack(pady=10)
+    entrada_ra = ctk.CTkEntry(app,placeholder_text='Digite seu RA')
+    entrada_ra.pack(pady=10)
+
+    #campo senha
+    ctk.CTkLabel(app,text='Senha:').pack(pady=10)
     entrada_senha = ctk.CTkEntry(app,placeholder_text='Digite sua senha')
     entrada_senha.pack(pady=10)
-    #button
-    botao = ctk.CTkButton(app,text='Logar',command=validar_login)
-    botao.pack(pady=10)
+
+    #botão de logar
+    botao_login = ctk.CTkButton(app,text='Logar',command=validar_login)
+    botao_login.pack(pady=10)
 
     # campo do feedback do login
     resultado_login = ctk.CTkLabel(app,text='')
     resultado_login.pack(pady=10)
 
+
     # inicia o loop da aplicação
     app.mainloop()
+
+# chamar função
+interface_login()
