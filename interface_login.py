42ea097f4557f35b355004e7745b1c127912f54c
CarlosEduardoRodrigu <caduka678@gmail.com>
2025-09-11 14:04:08 -0300

    -Check box movido para cima da caixa de redação
    -Verificação dos requisitos mínimos dos campos usuário e senha
    
    -Mensagem caso login bem sucedido, e mensagem quando usuário ou senha inválidos

--- interface_login.py
+++ interface_login.py
@@ -3,22 +3,27 @@ import customtkinter as ctk
 ctk.set_appearance_mode('dark')
 dados_login = {}
 
-def validar_login(usuario_entry, senha_entry, redacao_textbox, app):
+def validar_login(usuario_entry, senha_entry, redacao_textbox,resultado_login, app):
     usuario = usuario_entry.get().strip()
     senha = senha_entry.get().strip()
     redacao = redacao_textbox.get("1.0", "end-1c").strip()  # pega todoo o texto da redação
 
     if "-" in usuario:
         ra, digito = usuario.split("-")
+
+    if len(senha) >= 6 and 15 >= len(usuario) >= 10:
+            ra, digito = usuario[:-1], usuario[-1]
+            dados_login["ra"] = ra
+            dados_login["digito"] = digito
+            dados_login["senha"] = senha
+            dados_login["redacao"] = redacao  # armazena a redação
+
+            resultado_login.configure(text='Entrando na Sala do Futuro...',text_color='green')
+
+            app.destroy()
+
     else:
-        ra, digito = usuario[:-1], usuario[-1]
-
-    dados_login["ra"] = ra
-    dados_login["digito"] = digito
-    dados_login["senha"] = senha
-    dados_login["redacao"] = redacao  # armazena a redação
-
-    app.destroy()
+        resultado_login.configure(text='Login ou senha inválidos',text_color='red')
 
 def criar_janela():
     app = ctk.CTk()
@@ -36,31 +41,37 @@ def criar_janela():
     entrada_senha = ctk.CTkEntry(app, placeholder_text='Digite sua senha', show="*", width=280)
     entrada_senha.pack(pady=(0, 20))
 
-    # Redação
-    ctk.CTkLabel(app, text='Digite sua Redação:', font=('default', 12, 'bold')).pack(pady=(0,5))
-    redacao_textbox = ctk.CTkTextbox(app, width=350, height=200)
-    redacao_textbox.pack(pady=(0, 10))
-
-    # Checkbox
-    var_check = ctk.BooleanVar()
-    checkbox = ctk.CTkCheckBox( master=app,
-    text="Mostrar senha",
-    variable=var_check,
-    onvalue="on",
-    offvalue="off",
-    command=lambda: mostrar_senha())
-    checkbox.pack(pady=(0,10),anchor='w',padx=25)
-
+    # msotrar senha
     def mostrar_senha():
         if var_check.get():  # True se marcado
             entrada_senha.configure(show='')
         else:
             entrada_senha.configure(show='*')
 
+    # Checkbox
+    var_check = ctk.BooleanVar()
+    checkbox = ctk.CTkCheckBox(master=app,
+                               text="Mostrar senha",
+                               variable=var_check,
+                               onvalue=True,
+                               offvalue=False,
+                               command=mostrar_senha)
+    checkbox.pack(pady=(0, 10), anchor='w', padx=30)
+
+    # Redação
+    ctk.CTkLabel(app, text='Digite sua Redação:', font=('default', 12, 'bold')).pack(pady=(0,5))
+    redacao_textbox = ctk.CTkTextbox(app, width=350, height=200)
+    redacao_textbox.pack(pady=(0, 10))
+
+
     # Botão login
     ctk.CTkButton(app, text='Entrar',
-                  command=lambda: validar_login(entrada_usuario, entrada_senha, redacao_textbox, app),
+                  command= lambda:validar_login(entrada_usuario, entrada_senha, redacao_textbox, resultado_login, app),
                   border_width=2, border_color='#005180', width=280).pack()
+
+    # resulado login
+    resultado_login = ctk.CTkLabel(app,text='')
+    resultado_login.pack(pady=10)
 
     app.mainloop()
     return dados_login
