37d3134a1e3c4f5ca890df65b2b5943ab99c44ef
Gamerk <caduka678@gmail.com>
2025-09-10 21:14:52 -0300

    -Mudou os campos da "preenche o RA","Preenche o dígito","Preenche a senha", tirando as variáveis e transformando uma única linha

--- main.py
+++ main.py
@@ -4,24 +4,19 @@ from interface_login import criar_janela
 
 async def preencher_dados_estudante(pagina, ra, digito, senha):
     # Seleciona "Estudante"
-    clicar_sou_estudante = pagina.get_by_text('Estudante', exact=True)
-    await clicar_sou_estudante.click()
+    await pagina.get_by_text('Estudante', exact=True).click()
 
     # Preenche o RA
-    ra_input = pagina.get_by_role('textbox', name='RA')
-    await ra_input.fill(ra)
+    await pagina.get_by_role('textbox', name='RA').fill(ra)
 
     # Preenche o dígito
-    digito_input = pagina.get_by_role('textbox', name='Dígito')
-    await digito_input.fill(digito)
+    await pagina.get_by_role('textbox', name='Dígito').fill(digito)
 
     # Preenche a senha
-    senha_input = pagina.get_by_role('textbox', name='Digite sua senha')
-    await senha_input.fill(senha)
+    await pagina.get_by_role('textbox', name='Digite sua senha').fill(senha)
 
-    # TODO: clicar no botão "Acessar"
-    acessar_btn = pagina.get_by_role('button', name='Acessar')
-    await acessar_btn.click()
+    # Clica no botão "Acessar"
+    await pagina.get_by_role('button', name='Acessar').click()
 
 async def main():
     # Primeiro abre a interface e pega os dados
